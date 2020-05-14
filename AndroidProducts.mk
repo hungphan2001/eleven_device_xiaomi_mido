6348ad9bbd3c64e896651ba85c4d67f28dbb50ad
smokey18 <kmboplays@gmail.com>
2020-05-14 16:24:51 +0000

    Extendify...
    * Official Tag
    * nuke all LineageOS Stuff
    
    Signed-off-by: smokey18 <kmboplays@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_mido.mk
+    $(LOCAL_DIR)/exui_mido.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mido-user \
-    lineage_mido-userdebug \
-    lineage_mido-eng
+    exui_mido-eng \
+    exui_mido-userdebug \
+    exui_mido-user
